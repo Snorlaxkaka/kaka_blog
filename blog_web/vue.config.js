8f1974ad561ea185f58fbb35bf4792301e49ca02
linjunjie <3185058251@qq.com>
2024-12-08 13:50:22 +0800

    page部署_更改publicpath
    检查是否是publicpath弄错了才访问资源路径失败

--- blog_web/vue.config.js
+++ blog_web/vue.config.js
@@ -6,7 +6,7 @@ const path = require('path')
 module.exports = {
   lintOnSave: false,
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/web' : '/',
+  publicPath: '/',
   productionSourceMap: false,
   chainWebpack: (config) => {
     //最小化代码
